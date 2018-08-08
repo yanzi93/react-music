e3a389280f052d316378c55d815d54415aba2ac5
Swallow <33450933+yanzi93@users.noreply.github.com>
2018-08-08 23:07:15 +0800

    Add files via upload

---
+++ src/views/common/songlist.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { List } from 'antd-mobile'
+import {connect} from 'react-redux'
+import './songlist.css'
+
+const Item = List.Item;
+
+class SongList extends Component {
+  static defaultProps = {
+    songList:[]
+  };
+  static propTypes = {
+    songList: PropTypes.array.isRequired
+  };
+  render() {
+    let {songList} = this.props;
+    return (
+      <List className="common-list">
+      {
+        songList.map((item) => {
+          return (
+            <Item 
+              extra={<i className="iconfont icon-xiazai-copy" style={{'fontSize':'16px'}}></i>} 
+              key={item.hash}
+              onClick={()=>{
+                this.props.dispatch({type: 'updateHash', hash: item.hash});
+                this.props.dispatch({type: 'updateSongList', songList: songList});
+              }}
+            >
+              {item.filename}
+            </Item>)
+        })
+      }
+      </List>
+    );
+  }
+}
+
+export default connect()(SongList);
